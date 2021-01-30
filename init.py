17752deb34cd0abcc3a2ea4d62a6761c2dafc11a
Hansi Hundsberger <hansih2018@gmail.com>
2021-01-30 14:17:34 -0800

    Update init.py

--- init.py
+++ init.py
@@ -23,7 +23,7 @@ app.register_blueprint(auth_blueprint)
 from main import main as main_blueprint
 app.register_blueprint(main_blueprint)
 
-db.create_all(app=app)
+#db.create_all(app=app)
 
 def open_browser():
     url = "http://127.0.0.1:5000/"
