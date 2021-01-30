f50c821d4c08a3eba0bd8f6b919999bc2adc5374
Hansi Hundsberger <hansih2018@gmail.com>
2021-01-29 22:47:54 -0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,1 +1,16 @@
-print("hello motherfucker")
+from flask import Flask
+from threading import Timer
+import webbrowser
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Hey there!"
+
+def open_browser():
+    url = "http://127.0.0.1:5000/"
+    webbrowser.open_new_tab(url)
+
+if __name__ == '__main__':
+    Timer(1, open_browser).start()
+    app.run(debug=True)
