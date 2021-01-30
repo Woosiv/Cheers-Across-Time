db26b9f90854ea0fe3e1ee51f6c89f6392ce8875
Woosiv <williamhsiehoct@gmail.com>
2021-01-30 12:23:36 -0800

    Login page prototype

--- auth.py
+++ auth.py
@@ -1,25 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
 # Login Page
-@auth.route('/login', methods=['POST'])
+@auth.route('/login', methods=['POST', 'GET'])
 def login():
-    #if request.method == 'POST':
-    user = request.form['username']
-    password = request.form['password']
-    if verifyLogin(user, password):
-        print('correct user login')
-        return redirect(url_for('homepage', username = user))
+    if request.method == 'POST':
+        user = request.form['username']
+        password = request.form['password']
+        if verifyLogin(user, password):
+            print('correct user login')
+            return redirect(url_for('main.root', username = user))
+        else:
+            print('failed login')
+            return redirect(url_for('auth.login'))
     else:
-        print('failed login')
-        return redirect(url_for('login'))
-    #else:
-        #return app.send_static_file('login.html')
+        return render_template("login.html")
 
 # Verifies the login with the database
 def verifyLogin(username, password):
-    return username == 'fail'
+    return username != 'fail'
 
 @auth.route('/signup')
 def signup():
