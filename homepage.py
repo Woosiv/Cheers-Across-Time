917a58420e6629ac4e689e5e3b3687c79321f754
Hansi Hundsberger <hansih2018@gmail.com>
2021-01-29 23:22:13 -0800

    Homepage

--- homepage.py
+++ homepage.py
@@ -1,21 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 from threading import Timer
+import time
 import webbrowser
 
 app = Flask(__name__)
-# Home page
+#HomePage
 @app.route('/')
 def home():
-    return "Hey there!"
+    return render_template('home.html')
+
+def open_browser():
+    url = "http://127.0.0.1:5000/"
+    webbrowser.open_new_tab(url)
 
 # Login Page
 @app.route('/login')
 def login():
     return "Log in"
-
-def open_browser():
-    url = "http://127.0.0.1:5000/"
-    webbrowser.open_new_tab(url)
 
 if __name__ == '__main__':
     Timer(0, open_browser).start()
