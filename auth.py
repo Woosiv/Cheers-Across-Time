affbfb48c2f77cb092c5d3d3998aa7e6d8394578
RyanLam2000 <itsryanlam2000@gmail.com>
2021-01-30 17:22:19 -0800

    Added logout backend code

--- auth.py
+++ auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user
+from flask_login import login_user, logout_user
 from models import User
 from exts import db
 
@@ -59,3 +59,9 @@ def signup():
         return redirect(url_for('main.root'))
     else:
         return render_template('signup.html')
+
+@auth.route('/logout')
+@login_required
+def legout():
+    logout_user()
+    return redirect(url_for('main.root'))
